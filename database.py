7dcd1fbb3c8e0ff65ce5f692d7e86472afbf1926
OraSFit15 <orafitoussi@gmail.com>
2024-03-03 21:19:18 +0200

    "RMI-10 "

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ from pymongo.errors import PyMongoError
 import bcrypt
 import base64
 
-"""change participant from patient  """
+"""change participant from patient"""
 class MongoDB:
     """
     A MongoDB database wrapper.
